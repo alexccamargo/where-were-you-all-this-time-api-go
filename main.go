23f4ba6a745a8d9629b518c998b45a8ab43c1c24
Alex Carvalho Camargo <Alex.Camargo@londonlife.com>
2020-02-25 18:40:41 -0500

    added goDotenv

--- main.go
+++ main.go
@@ -5,17 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func goDotEnvVariable(key string) string {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+	fmt.Fprint(w, "<h1>Hello, World!</h1>")
+	fmt.Fprint(w, goDotEnvVariable("MY_VAR"))
 }
 
 func main() {
+
 	http.HandleFunc("/", indexHandler)
 
 	port := os.Getenv("PORT")
